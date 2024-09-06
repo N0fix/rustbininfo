b68880bf09b080c91c0a7a4f4ad860f3a85deb59
Nofix <16479266+N0fix@users.noreply.github.com>
2024-09-06 12:17:49 +0200

    Added commandline option and improved deps

--- src/rustbininfo/main.py
+++ src/rustbininfo/main.py
@@ -18,35 +18,6 @@ example_text = r"""Usage examples:
  rbi -d 'challenge.exe'
 """
 
-
-def extract_tarfile(tar_path: pathlib.Path) -> pathlib.Path:
-    """Extract given tarfile.
-
-    Args:
-        tar_path (pathlib.Path): Caller must ensure that file exists. Filename must have
-        one of the following extensions: '.gz', '.tar', '.tgz'.
-
-    Returns:
-        pathlib.Path: directory where files got extracted.
-    """
-    assert tar_path.exists()
-
-    tar = tarfile.open(tar_path)
-    tar.extractall(path=tar_path.parent)
-    tar.close()
-
-    if ".gz" in tar_path.suffixes:
-        tar_path = tar_path.with_suffix("")
-
-    if ".tar" in tar_path.suffixes:
-        tar_path = tar_path.with_suffix("")
-
-    if ".tgz" in tar_path.suffixes:
-        tar_path = tar_path.with_suffix("")
-
-    return tar_path
-
-
 def parse_args():
     # Main parser
     parser = ArgumentParser(
@@ -85,26 +56,9 @@ def main_cli():
         t = TargetRustInfo.from_target(args.target)
         min_date, max_date = get_min_max_update_time(t.dependencies)
         print(f"Latest dependency was added between {min_date} and {max_date}")
-        exit(1)
+        sys.exit(1)
 
-    # if args.mode == "info":
     print(TargetRustInfo.from_target(args.target, not args.full))
-
-    # elif args.mode == "download":
-    #     c = Crate.from_depstring(args.crate, fast_load=False)
-    #     try:
-    #         result = c.download(args.directory)
-
-    #     except InvalidVersionError:
-    #         print("Version of your crate does not exists on crates.io")
-    #         exit(1)
-
-    #     print(f"{c} downloaded to {result}")
-
-    #     if args.extract:
-    #         print(f"Extracted to {extract_tarfile(pathlib.Path(result))}")
-
-
 
 if __name__ == "__main__":
     main_cli()
